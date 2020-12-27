8c98c30ead6b57a87e69c36352f6274767487d67
Stacey Ell <stacey.ell@gmail.com>
2020-12-27 15:34:17 -0700

    Use remote tarball

--- inspircd/build.nix
+++ inspircd/build.nix
@@ -4,7 +4,10 @@ derivation {
   builder = "${bash}/bin/bash";
   args = [ ./builder.sh ];
   buildInputs = [ gnutar gzip gnumake gcc binutils-unwrapped coreutils gawk gnused gnugrep perl openssl openssl.dev which pkgconfig findutils bash ];
-  src = ./inspircd-3.8.1.tar.gz;
+  src = fetchurl {
+    sha256 = "0jxlh7ip5pd6fp8axwy4b35gn3v908726l5nfhgjwf4vyd1cls10";
+    url = "https://yyc.mediarepo.yshi.org/sources/tarballs/inspircd-3.8.1.tar.gz";
+  };
   system = builtins.currentSystem;
 }
 
